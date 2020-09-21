3f53d4d3fbd598fc582f1c8e8502156f13d81da6
Thiago Tavares <thiagooo15@gmail.com>
2020-09-21 19:18:56 -0300

    começando exercicios

---
+++ Bloco-26-Node/26-1/index.js
@@ -0,0 +1,6 @@
+const readline = require('readline-sync');
+
+function executaCalculo() {
+  const altura = readline.questionFloat("digita a Alutura:").toFixed(2);
+  const peso = readline.questionFloat("digite o peso:").toFixed(2);
+}
